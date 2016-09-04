d396d277029d30ad6dea252a1b5833ccacadea43
Alex DeCapri <alexdecapri@gmail.com>
2016-09-04 16:17:14 -0600

    sample axios.get set up and retrieving data

--- src/containers/SpotifyContainer.js
+++ src/containers/SpotifyContainer.js
@@ -1,6 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const BASE_URL = 'https://api.spotify.com/v1/search?type=track&';
 
 class SpotifyContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      songs: []
+    }
+  }
+
+  songSearch(term) {
+    return axios.get(`${BASE_URL}q=${term}`)
+    .then((response) => {
+      console.log(response.data);
+      this.setState({
+        songs: response.data.tracks.items
+      });
+      console.log(this.state.songs);
+    })
+  }
+
+  componentDidMount() {
+    this.songSearch('rihanna');
+  }
+
   render() {
     return (
       <div>

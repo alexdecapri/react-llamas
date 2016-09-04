721f10aa4d7447a266c711cd70ada5b494317fd5
Alex DeCapri <alexdecapri@gmail.com>
2016-09-04 15:19:16 -0600

    add facts.js, refactor FactsContainer by moving bulk of content into facts.js

---
+++ src/components/facts.js
@@ -0,0 +1,100 @@
+import React from 'react';
+
+function Facts () {
+  return (
+    <div>
+      <div className="media">
+        <a className="media-left" href="#">
+          <div className="ll-container"><img className="media-object ll-thumbnail" src="src/images/1.jpg" alt="" /></div>
+        </a>
+        <div className="media-body">
+          <h4 className="media-heading">Fact #1</h4>
+          The llama is the South American relative of the camel.
+        </div>
+      </div>
+      <div className="media">
+        <a className="media-left" href="#">
+          <div className="ll-container"><img className="media-object ll-thumbnail" src="src/images/2.jpg" alt="" /></div>
+        </a>
+        <div className="media-body">
+          <h4 className="media-heading">Fact #2</h4>
+          Correcting a common misbelief, llamas actualy do NOT have humps.
+        </div>
+      </div>
+      <div className="media">
+        <a className="media-left" href="#">
+          <div className="ll-container"><img className="media-object ll-thumbnail" src="src/images/3.jpg" alt="" /></div>
+        </a>
+        <div className="media-body">
+          <h4 className="media-heading">Fact #3</h4>
+          Making them very useful animals, llamas don't require a lot of water.
+        </div>
+      </div>
+      <div className="media">
+        <a className="media-left" href="#">
+          <div className="ll-container"><img className="media-object ll-thumbnail" src="src/images/4.png" alt="" /></div>
+        </a>
+        <div className="media-body">
+          <h4 className="media-heading">Fact #4</h4>
+          Llamas have been domesticated and used by the peoples of the Andes Mountains for centuries.
+        </div>
+      </div>
+      <div className="media">
+        <a className="media-left" href="#">
+          <div className="ll-container"><img className="media-object ll-thumbnail" src="src/images/5.jpg" alt="" /></div>
+        </a>
+        <div className="media-body">
+          <h4 className="media-heading">Fact #5</h4>
+          Llamas serve people primarily to carry things across rough terrain.  The average llama is saddled with loads of 50-75 pounds.
+        </div>
+      </div>
+      <div className="media">
+        <a className="media-left" href="#">
+          <div className="ll-container"><img className="media-object ll-thumbnail" src="src/images/6.jpg" alt="" /></div>
+        </a>
+        <div className="media-body">
+          <h4 className="media-heading">Fact #6</h4>
+          Llamas can cover up to 20 miles in a single day, but an overloaded llama will simply refuse to move.
+        </div>
+      </div>
+      <div className="media">
+        <a className="media-left" href="#">
+          <div className="ll-container"><img className="media-object ll-thumbnail" src="src/images/7.jpg" alt="" /></div>
+        </a>
+        <div className="media-body">
+          <h4 className="media-heading">Fact #7</h4>
+          When these animals are angry, they like to spit, hiss, and even kick at their owners.
+        </div>
+      </div>
+      <div className="media">
+        <a className="media-left" href="#">
+          <div className="ll-container"><img className="media-object ll-thumbnail" src="src/images/8.jpg" alt="" /></div>
+        </a>
+        <div className="media-body">
+          <h4 className="media-heading">Fact #8</h4>
+          Their diet consists of grass, which they regurgitate and chew as cud until they are ready to fully digest.
+        </div>
+      </div>
+      <div className="media">
+        <a className="media-left" href="#">
+          <div className="ll-container"><img className="media-object ll-thumbnail" src="src/images/9.jpg" alt="" /></div>
+        </a>
+        <div className="media-body">
+          <h4 className="media-heading">Fact #9</h4>
+          Leather is made from their hides, and their wool is crafted into ropes, rugs, and fabrics.
+        </div>
+      </div>
+      <div className="media">
+        <a className="media-left" href="#">
+          <div className="ll-container"><img className="media-object ll-thumbnail" src="src/images/10.jpg" alt="" /></div>
+        </a>
+        <div className="media-body">
+          <h4 className="media-heading">Fact #10</h4>
+          Llama excrement is dried and burned for fuel. That probably stinks like poop!
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Facts;

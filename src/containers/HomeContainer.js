6b12296ec9e56f3599fd426b7d51b486f9054511
Alex DeCapri <alexdecapri@gmail.com>
2016-09-04 14:13:19 -0600

    buttons link to correct views

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class HomeContainer extends Component {
   render() {
@@ -6,8 +7,8 @@ class HomeContainer extends Component {
       <div className="text-center">
         <h1 className="text-center">Welcome to React Llamas</h1>
         <h3>A quick reference for all you would (n)ever need to know about these majestic animals.</h3>
-        <button className="btn btn-primary">Show me some facts!</button>
-        <button className="btn btn-primary">I'm already a llama expert- what else ya got?</button>
+        <Link to="facts"><button className="btn btn-primary">Show me some facts!</button></Link>
+        <Link to="playlist"><button className="btn btn-primary">I'm already a llama expert- what else ya got?</button></Link>
       </div>
     )
   }

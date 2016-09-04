86dcceb2b4e4d81d3802a0c411989e6a81cb38c0
Alex DeCapri <alexdecapri@gmail.com>
2016-09-04 12:52:35 -0600

    create routes.js, render initial route

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HomeContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h2 className="text-center">React Llamas</h2>
+        <p>A bunch of text about llamas...</p>
+      </div>
+    )
+  }
+}
+
+export default HomeContainer;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import routes from './routes';
+
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  <Router history={browserHistory} routes={routes} />
+  , document.querySelector('.container')
+);

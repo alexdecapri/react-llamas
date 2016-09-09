1925a16284f0d577b6599b87ebc1d850e35f0c7c
Alex DeCapri <alexdecapri@gmail.com>
2016-09-09 17:14:30 -0600

    add about route

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,13 @@ import App from './components/app';
 import HomeContainer from './containers/HomeContainer';
 import FactsContainer from './containers/FactsContainer';
 import SpotifyContainer from './containers/SpotifyContainer';
+import AboutContainer from './containers/AboutContainer';
 
 export default (
   <Route path='/' component={App}>
       <IndexRoute component={HomeContainer} />
     <Route path="facts" component={FactsContainer} />
     <Route path="playlist" component={SpotifyContainer} />
+    <Route path="about" component={AboutContainer} />
   </Route>
 )

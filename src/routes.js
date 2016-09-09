45c86fb6111ff7d5d18c7a2ae4424aa5f77689b8
Alex DeCapri <alexdecapri@gmail.com>
2016-09-09 14:06:38 -0600

    delete bg_image_wrapper

--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,10 @@ import App from './components/app';
 import HomeContainer from './containers/HomeContainer';
 import FactsContainer from './containers/FactsContainer';
 import SpotifyContainer from './containers/SpotifyContainer';
-import BGImageWrapper from './components/bg_image_wrapper';
 
 export default (
   <Route path='/' component={App}>
-    <Route component={BGImageWrapper}>
       <IndexRoute component={HomeContainer} />
-    </Route>
     <Route path="facts" component={FactsContainer} />
     <Route path="playlist" component={SpotifyContainer} />
   </Route>
